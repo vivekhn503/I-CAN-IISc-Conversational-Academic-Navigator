0cca0c3dc4097105ef561155c94b41b71af0ec12
PrasannaBV <puru.bv@gmail.com>
2025-06-08 12:17:57 +0530

    Update app.py

--- src/ui/app.py
+++ src/ui/app.py
@@ -1,8 +1,13 @@
 import streamlit as st
 
 def main():
-    st.title("Hello, World!")
-    st.write(" setup is working 🎉")
+    st.title("I-CAN\nIISc-Conversational-Academic-Navigator")
+
+  
+    st.subheader("Ask a question")
+    q = st.text_input("Your question:")
+    if st.button("Ask") and q:
+        st.write("Answer")
 
 if __name__ == "__main__":
     main()
