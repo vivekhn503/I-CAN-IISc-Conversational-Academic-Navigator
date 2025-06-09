0b6a41e429518d6333d2e1a9bbdf617fbb9f3427
PrasannaBV <puru.bv@gmail.com>
2025-06-10 00:25:52 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="ican_navigator",              
+    version="0.1.0",
+    package_dir={"": "src"},            
+    packages=find_packages(where="src"),
+    install_requires=[
+        # copy  runtime deps from requirements.txt, e.g.:
+        "streamlit",
+        "langchain"
+    ],
+)
