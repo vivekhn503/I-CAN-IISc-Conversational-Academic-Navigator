90d7436db794aa476258b3e3ee3aec1ff1224e28
PrasannaBV <puru.bv@gmail.com>
2025-06-10 23:34:16 +0530

    Update settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,1 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CHUNK_SIZE     = int(os.getenv("CHUNK_SIZE", 300))
+CHUNK_OVERLAP  = int(os.getenv("CHUNK_OVERLAP", 50))
+MODEL_NAME     = os.getenv("MODEL_NAME", "deepseek-r1:latest")
+TEMPERATURE    = float(os.getenv("TEMPERATURE", 0.4))
 
