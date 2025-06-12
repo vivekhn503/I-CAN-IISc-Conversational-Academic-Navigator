b981ef8567860753efe3fe94461ed446ce61de0d
PrasannaBV <puru.bv@gmail.com>
2025-06-12 20:26:12 +0530

    Update settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -5,6 +5,9 @@ load_dotenv()
 
 CHUNK_SIZE     = int(os.getenv("CHUNK_SIZE", 300))
 CHUNK_OVERLAP  = int(os.getenv("CHUNK_OVERLAP", 50))
-MODEL_NAME     = os.getenv("MODEL_NAME", "deepseek-r1:latest")
+MODEL_NAME     = os.getenv("MODEL_NAME", "gpt-4o-mini")
 TEMPERATURE    = float(os.getenv("TEMPERATURE", 0.4))
+os.environ["OPENAI_API_KEY"] = ""
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FAISS_INDEX_PATH = os.path.join(BASE_DIR, "../data/faiss_index") 
 
